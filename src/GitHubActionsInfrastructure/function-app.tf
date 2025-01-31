1880b4a97b4df659f6ccd3939039e45225cf294e
lmcwhirt-insight <les.mcwhirter@insight.com>
2025-01-31 10:00:27 -0700

    Reverting nuget version code

--- src/GitHubActionsInfrastructure/function-app.tf
+++ src/GitHubActionsInfrastructure/function-app.tf
@@ -8,17 +8,17 @@ resource "azurerm_service_plan" "function-sp" {
   sku_name            = "Y1"
 }
 
-# resource "azurerm_linux_function_app" "webapi-fa" {
-#   name                       = format("%s-%s", local.base-name, "fa")
-#   resource_group_name        = azurerm_resource_group.deployment-rg[0].name
-#   location                   = azurerm_resource_group.deployment-rg[0].location
-#   service_plan_id            = azurerm_service_plan.function-sp.id
-#   storage_account_name       = azurerm_storage_account.function-app-sa.name
-#   storage_account_access_key = azurerm_storage_account.function-app-sa.primary_access_key
+resource "azurerm_linux_function_app" "webapi-fa" {
+  name                       = format("%s-%s", local.base-name, "fa")
+  resource_group_name        = azurerm_resource_group.deployment-rg[0].name
+  location                   = azurerm_resource_group.deployment-rg[0].location
+  service_plan_id            = azurerm_service_plan.function-sp.id
+  storage_account_name       = azurerm_storage_account.function-app-sa.name
+  storage_account_access_key = azurerm_storage_account.function-app-sa.primary_access_key
 
-#   site_config {
-#     application_stack {
-#       dotnet_version = "8.0"
-#     }
-#   }
-# }
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+    }
+  }
+}
