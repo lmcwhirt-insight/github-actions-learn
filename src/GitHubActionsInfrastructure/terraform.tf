c826cd188e93dee576567fc970ac91a2b47ce6bc
lmcwhirt-insight <les.mcwhirter@insight.com>
2025-01-31 15:02:43 -0700

    TF backend

--- src/GitHubActionsInfrastructure/terraform.tf
+++ src/GitHubActionsInfrastructure/terraform.tf
@@ -5,13 +5,13 @@ terraform {
       version = ">=4.16.0"
     }
   }
-}
 
-#   backend "azurerm" {
-#     resource_group_name  = "my-tfstate-rg"
-#     storage_account_name = "mytfstateaccount"
-#     container_name       = "tfstate"
-#     key                  = "terraform.tfstate"
-#   }
+  backend "azurerm" {
+    resource_group_name  = "tf-state-rg"
+    storage_account_name = "satfstateexample"
+    container_name       = "ghal-tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
 
 # terraform required_providers azurerm
