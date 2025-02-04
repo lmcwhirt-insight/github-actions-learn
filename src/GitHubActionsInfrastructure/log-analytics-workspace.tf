536ac3088112bf4a7110430611a7a937667d2e0e
lmcwhirt-insight <les.mcwhirter@insight.com>
2025-02-03 17:35:52 -0700

    Fixed retention

--- src/GitHubActionsInfrastructure/log-analytics-workspace.tf
+++ src/GitHubActionsInfrastructure/log-analytics-workspace.tf
@@ -5,5 +5,5 @@ resource "azurerm_log_analytics_workspace" "law" {
   resource_group_name = azurerm_resource_group.deployment-rg[0].name
   location            = azurerm_resource_group.deployment-rg[0].location
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
